99fbc843735619134c97bf16f33e5c4937949991
Ricardo Fernandez <ricardo.a.fernandez.a@gmail.com>
2023-08-29 12:20:55 -0600

    Initial Commit

---
+++ Pokedex/PokedexApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokedexApp.swift
+//  Pokedex
+//
+//  Created by Ricardo Adolfo Fernández Alvarado on 29/08/23.
+//
+
+import SwiftUI
+
+@main
+struct PokedexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
